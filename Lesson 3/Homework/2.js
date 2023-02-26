751f4892a9b7966e67cb6ffe5c80a24e2acbce70
Alexander <asukhushin@icloud.com>
2023-02-26 13:38:26 +0300

    Lecture 3, Seminar 3, Homework 3

---
+++ Lesson 3/Homework/2.js
@@ -0,0 +1,9 @@
+function greeting(your_name) {
+    console.log("Приветствую, " + your_name + "!");
+    return your_name
+}
+
+let your_name = prompt("Введите ваше имя:");
+greeting(your_name);
+
+// alert("Приветствую, " + your_name + "!");
